5c856094a401f2ad064fcf4a08a645f73545fd85
Anfizer11 <anfizer1@gmail.com>
2026-08-24 12:05:13 -0400

    file updates

---
+++ simulation/controllers/safenav_perfect_nav_controller/safenav_perfect_nav_controller.py
@@ -0,0 +1,14 @@
+import csv
+import math
+from datetime import datetime
+from pathlib import Path
+
+from controller import Robot, Supervisor
+
+
+# ============================================================
+# CONFIGURATION
+# ============================================================
+
+TIME_STEP = int(supervisor.getBasicTimeStep())
+
